a610d6e9c884e9a7c205ce05e118dd3b43a27e2e
boy-son <branden.wheat@yahoo.com>
2023-03-09 12:27:41 -0500

    added to database and started on install js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,12 +14,25 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-jate = 
+const jateDB = await openDB('jate', 1);
+const txt = jateDB.transaction('jate', 'readwrite');
+const store = txt.objectStore('jate');
+const reqPut = store.put({id: 1, value: content});
+const resPut = await reqPut;
+console.log("data added: ", resPut.value);
 };
 
   console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+const jateDB = await openDB('jate', 1);
+const txt = jateDB.transaction('jate', 'readonly');
+const store = txt.objectStore('jate');
+const reqGet = store.get(1);
+const resGet = await reqGet;
+console.log("data retrieved: ", resGet.value);
+return resGet.value;
+};
 
 initdb();
